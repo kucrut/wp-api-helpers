142cba6a2a885b9fdf8554d425f3d66df6aa92d2
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-13 14:23:07 +0700

    Generate types for jwt_login()

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,6 +10,21 @@ declare module '@kucrut/wp-api-helpers' {
 	 * @return WordPress API root URL.
 	 */
 	export function discover(url: string): Promise<string>;
+	/**
+	 * Log in to WordPress via JWT
+	 *
+	 * @since 0.1.0
+	 *
+	 * @see {@link https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/}
+	 *
+	 * @param {Object} credentials Credentials.
+	 * @return Fetch response.
+	 */
+	export function jwt_login({ url, username, password }: {
+		url: string;
+		username: string;
+		password: string;
+	}): Promise<Response>;
 	export const jwt_login_data: z.ZodObject<{
 		user_email: z.ZodString;
 		user_display_name: z.ZodString;
