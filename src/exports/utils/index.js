6e38ee006bc28da3cbc8eb81b81d72f368863494
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-13 18:17:58 +0700

    Add util: handle_response()

--- src/exports/utils/index.js
+++ src/exports/utils/index.js
@@ -1,3 +1,4 @@
+import { rest_error_schema } from '../wp/schema.js';
 import { ZodError } from 'zod';
 
 /**
@@ -27,4 +28,53 @@ export function get_error_message( error, fallback, dump = true ) {
 	}
 
 	return message;
+}
+
+/**
+ * Handle WP REST API response
+ *
+ * This helps catch syntax errors in json because of PHP notices, etc.
+ *
+ * @since 0.0.1
+ *
+ * @template T
+ *
+ * @param {Response} response Fetch response object.
+ * @param {import('$types').HandleResponse<T>} callback Callback to run when json is valid.
+ *
+ * @throws {Error} JSON.parse error.
+ *
+ * @return {Promise<T>} Whatever the callback returns.
+ */
+export async function handle_response( response, callback ) {
+	const clone = response.clone();
+	let result;
+
+	if ( response.ok ) {
+		try {
+			result = await response.json();
+		} catch ( error ) {
+			const text = await clone.text();
+			const message = get_error_message( error, 'Please consult the logs.' ).replace( '<', '&lt;' );
+
+			throw new Error( `Unexpected response: ${ message }\n${ text }` );
+		}
+
+		return await callback( result );
+	}
+
+	const json = await response.json();
+	const error = rest_error_schema.safeParse( json );
+	/** @type {string} */
+	let message;
+
+	if ( error.success ) {
+		message = error.data.message;
+	} else {
+		message = 'Unexpected response from server. Please consult the logs.';
+		// eslint-disable-next-line no-console
+		console.error( error );
+	}
+
+	throw new Error( message );
 }
