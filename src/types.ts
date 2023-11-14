81d51ad1139af92c7e160232c6192a92abdfb999
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-15 00:04:49 +0700

    Add type def: HandledFetch

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
 /* eslint-disable no-unused-vars */
 
 export type HandleResponse< T > = ( data: unknown ) => Promise< T >;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type HandledFetch< F extends ( ...args: any ) => any, T > = (
+	...args: Parameters< F >
+) => ReturnType< HandleResponse< T > >;
