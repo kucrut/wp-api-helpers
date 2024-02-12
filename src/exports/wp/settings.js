9dc2904232826157ec73a2814ce324f0e588ad62
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-12 23:41:46 +0700

    Settings: Move schema def

--- src/exports/wp/settings.js
+++ src/exports/wp/settings.js
@@ -1,5 +1,28 @@
+import { z } from 'zod';
 import { fetch_and_parse, fetch_data } from '../utils/index.js';
-import { settings } from './schema.js';
+
+export const settings = z.object( {
+	date_format: z.string(),
+	default_category: z.number(),
+	default_comment_status: z.enum( [ 'open', 'closed' ] ),
+	default_ping_status: z.enum( [ 'open', 'closed' ] ),
+	default_post_format: z.string(),
+	description: z.string(),
+	email: z.string().email(),
+	language: z.string(),
+	page_for_posts: z.number(),
+	page_on_front: z.number(),
+	posts_per_page: z.number(),
+	show_on_front: z.enum( [ 'page', 'posts' ] ),
+	site_icon: z.number().nullable(),
+	site_logo: z.number().nullable(),
+	start_of_week: z.number(),
+	time_format: z.string(),
+	timezone: z.string(),
+	title: z.string(),
+	url: z.string(),
+	use_smilies: z.boolean(),
+} );
 
 /**
  * Get settings
