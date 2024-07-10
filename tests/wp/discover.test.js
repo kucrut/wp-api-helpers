edba3097c8b36b341bf7f8a8ee67cc03af60bec5
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-07-10 20:54:11 +0700

    Silent harmless eslint error

--- tests/wp/discover.test.js
+++ tests/wp/discover.test.js
@@ -16,6 +16,7 @@ const make_fail_test = async init => {
 	try {
 		await discover( 'boo' );
 		has_error = false;
+	// eslint-disable-next-line no-unused-vars
 	} catch ( error ) {
 		has_error = true;
 	}
